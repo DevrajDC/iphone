c201f034d4944e189b37f1b5abd1c33239cbe3c9
DevrajDC <devrajchatribin9978@gmail.com>
2024-05-06 01:28:43 +0530

    Footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import HowItWorks from "./components/HowItWorks";
@@ -16,6 +17,7 @@ function App() {
       <Model />
       <Features />
       <HowItWorks />
+      <Footer />
     </main>
   );
 }
