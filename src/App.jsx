dacb1d424b6652ec7837ca4c50437f5b00f68f7f
DevrajDC <devrajchatribin9978@gmail.com>
2024-05-06 01:22:41 +0530

    Completed How it Works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
@@ -14,6 +15,7 @@ function App() {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 }
