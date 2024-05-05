9e99e1663188eb7c4e39be14befa3726f18a380b
DevrajDC <devrajchatribin9978@gmail.com>
2024-05-06 01:22:22 +0530

    fix max width in features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -35,7 +35,7 @@ const Features = () => {
 
   return (
     <section className="h-full common-padding bg-zinc relative overflow-hidden">
-      <div className="screen-max-wdith">
+      <div className="screen-max-width">
         <div className="mb-12 w-full">
           <h1 id="features_title" className="section-heading">
             Explore the full story.
