301b7dcda201ae4af19dad3e97a54c3cb5e5bc46
DevrajDC <devrajchatribin9978@gmail.com>
2024-05-06 02:07:50 +0530

    Fix highlights section animations

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -3,15 +3,16 @@ import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
 
 import VideoCarousel from "./VideoCarousel";
+import { animateWithGsap } from "../utils/animations";
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", {
+    animateWithGsap("#title", { opacity: 1, y: 0 });
+    animateWithGsap(".link", {
       opacity: 1,
       y: 0,
       delay: 0.5,
-      duration: 0.5,
+      // duration: 0.5,
       stagger: 0.1,
     });
   }, []);
@@ -28,11 +29,11 @@ const Highlights = () => {
           </h1>
 
           <div className="flex flex-wrap items-end gap-5">
-            <p className="link">
+            <p className="link opacity-0">
               Watch the film
               <img src={watchImg} alt="watch" className="ml-2" />
             </p>
-            <p className="link">
+            <p className="link opacity-0">
               Watch the event
               <img src={rightImg} alt="right" className="ml-2" />
             </p>
