b61d90d56ffdfc812dcf1ba64b96da314714f16b
DevrajDC <devrajchatribin9978@gmail.com>
2024-05-05 20:52:49 +0530

    Configuring Sentry for website performance tracking

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
+import * as Sentry from "@sentry/react";
+
 function App() {
   return (
     <main className="bg-black">
@@ -14,4 +16,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
